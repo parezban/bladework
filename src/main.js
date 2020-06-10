3649a802c410fd040a5521ae618ff9c7003af940
parezban <parezban@gmail.com>
2020-06-10 15:26:23 +0430

    add some base files

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import foo from './foo.js';
+export default function () {
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+  console.log(foo);
+}
